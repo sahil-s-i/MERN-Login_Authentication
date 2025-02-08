991c8b6a5198511286615d7694e73c614791c951
Sahil <sahilismailnavar@gmail.com>
2025-02-08 23:14:40 +0530

    Added app using components

--- server/server.js
+++ server/server.js
@@ -6,3 +6,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cookieParser);
+app.use(cors({credentials:true}));
